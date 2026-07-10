6bf2cc9b8954eb1181ce95430da6c2e04c1ea8dd
aozhan001 <aozhan001@example.com>
2026-07-10 23:14:27 +0800

    Add SNS module for PromptKD

--- scripts/promptkd/base2new_train.sh
+++ scripts/promptkd/base2new_train.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 
+# Example:
+# sh scripts/promptkd/base2new_train.sh dtd 1 0 0.2 0.25 4 0.5 32 0 8192
+
 # custom config
 DATA="/data2/workspace_hyw/promptkd/promptkd_data"
 TRAINER=PromptKD
 
 DATASET=$1 # 'imagenet' 'caltech101' 'dtd' 'eurosat' 'fgvc_aircraft' 'oxford_flowers' 'food101' 'oxford_pets' 'stanford_cars' 'sun397' 'ucf101'
 SEED=$2
-GPU_ID=$3
-MTP_ALPHA=$4
-DVP_ALPHA=$5
-PRIOR_GAMMA=$6
-KD_WEIGHT=${7:-}
-MTP_ENABLE=${8:-True}
-DVP_ENABLE=${9:-True}
-PRIOR_ENABLE=${10:-True}
+GPU_ID=${3:-0}
+DVP_ALPHA=${4:-0.2}
+PRIOR_GAMMA=${5:-0.5}
+SNS_RANK=${6:-4}
+SNS_RHO=${7:-0.5}
+SNS_PCA_RANK=${8:-32}
+SNS_SEMANTIC_RANK=${9:-0}
+SNS_MAX_SAMPLES=${10:-8192}
+KD_WEIGHT=${11:-}
 
-#dtd:0.3 0.2 0.25
-#oxford_flowers:0.2 0.2 0.25
-#eurosat:0.2 0.2 1
-
-
-#vit_b16_c2_ep20_batch8_4+4ctx_mgkd
 CFG=vit_b16_c2_ep20_batch8_4+4ctx
 SHOTS=0
 
-DIR=output/base2new/train_base/${DATASET}/${DATASET}_${MTP_ALPHA}_${DVP_ALPHA}_${PRIOR_GAMMA}/shots_${SHOTS}/${TRAINER}/${CFG}/seed_${SEED}
+DIR=output/base2new/train_base/${DATASET}/dvp${DVP_ALPHA}_prior${PRIOR_GAMMA}_snsr${SNS_RANK}_rho${SNS_RHO}_pca${SNS_PCA_RANK}_sem${SNS_SEMANTIC_RANK}/shots_${SHOTS}/${TRAINER}/${CFG}/seed_${SEED}
 
-# fgvc_aircraft, oxford_flowers, dtd: KD_WEIGHT:200
-# imagenet, caltech101, eurosat, food101, oxford_pets, stanford_cars, sun397, ucf101, KD_WEIGHT:1000
-
+# fgvc_aircraft, oxford_flowers, dtd: KD_WEIGHT=200
+# imagenet, caltech101, eurosat, food101, oxford_pets, stanford_cars, sun397, ucf101: KD_WEIGHT=1000
 if [ -z "${KD_WEIGHT}" ]; then
     case "${DATASET}" in
         fgvc_aircraft|oxford_flowers|dtd)
@@ -54,11 +51,14 @@ CUDA_VISIBLE_DEVICES=${GPU_ID} python train.py \
     TRAINER.MODAL base2novel \
     TRAINER.PROMPTKD.TEMPERATURE 1.0 \
     TRAINER.PROMPTKD.KD_WEIGHT ${KD_WEIGHT} \
-    TRAINER.PROMPTKD.USE_MULTI_TEMPLATE_TEXT ${MTP_ENABLE} \
-    TRAINER.PROMPTKD.DVP_ENABLE ${DVP_ENABLE} \
-    TRAINER.PROMPTKD.PRIOR_CORRECT ${PRIOR_ENABLE}\
-    TRAINER.PROMPTKD.MTP_ALPHA ${MTP_ALPHA}\
-    TRAINER.PROMPTKD.DVP_ALPHA ${DVP_ALPHA}\
-    TRAINER.PROMPTKD.PRIOR_GAMMA ${PRIOR_GAMMA}
-
-    
+    TRAINER.PROMPTKD.USE_MULTI_TEMPLATE_TEXT False \
+    TRAINER.PROMPTKD.DVP_ENABLE True \
+    TRAINER.PROMPTKD.PRIOR_CORRECT True \
+    TRAINER.PROMPTKD.SNS_ENABLE True \
+    TRAINER.PROMPTKD.DVP_ALPHA ${DVP_ALPHA} \
+    TRAINER.PROMPTKD.PRIOR_GAMMA ${PRIOR_GAMMA} \
+    TRAINER.PROMPTKD.SNS_RANK ${SNS_RANK} \
+    TRAINER.PROMPTKD.SNS_RHO ${SNS_RHO} \
+    TRAINER.PROMPTKD.SNS_PCA_RANK ${SNS_PCA_RANK} \
+    TRAINER.PROMPTKD.SNS_SEMANTIC_RANK ${SNS_SEMANTIC_RANK} \
+    TRAINER.PROMPTKD.SNS_MAX_SAMPLES ${SNS_MAX_SAMPLES}
